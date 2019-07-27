5ad509c461795499ceb7ccf7a9d0604924d2c18e
Jonathan Lin <jonathan.lin.703@gmail.com>
2019-07-27 14:11:41 -0700

    caching

--- food_spending.py
+++ food_spending.py
@@ -2,6 +2,7 @@ import googutils
 import argparse
 from datetime import datetime, date, timedelta
 import json
+import os
 
 SPREADSHEET_ID = '1rzlwFJGb0E332vyNzRVB72BGIwmzGPDl9QaCtDYtWyk'
 CURRENT_SHEET = 'Su19 Data'
@@ -9,40 +10,36 @@ ENTRIES_START_ROW = 9
 DATES_RANGE = 'B{}:B'.format(ENTRIES_START_ROW)
 STATS_RANGE = 'G3:G5'
 DESCRIPTORS = ('TODAY', 'YESTERDAY', 'TOMORROW')
+DATES_CACHE = 'cached/dates.json'
+STATS_CACHE = 'cached/stats.json'
 
 class FoodSpending():
     def __init__(self, get_stats=True):
         self.sheet_service = googutils.get_google_service('https://www.googleapis.com/auth/drive',
-                                                 'tokens/drive_full', 'sheets', 'v4', flags=args)
-        ranges = []
-        ranges.append(DATES_RANGE)
+                                                 'tokens/drive_full', 'sheets', 'v4')
+        with open(DATES_CACHE, 'r') as f:
+            self.dates = json.load(f)
         if get_stats:
-            ranges.append(STATS_RANGE)
-        results = googutils.batch_read_from_spreadsheet(SPREADSHEET_ID, ranges, self.sheet_service, majorDimension='COLUMNS')
-        self.dates = results[0][0]
-        if get_stats:
-            self.stats = [float(r[1:]) for r in results[1][0]]
+            if os.path.exists(STATS_CACHE):
+                with open(STATS_CACHE) as f:
+                    self.stats = json.load(f)
+            else:
+                self.stats = self.get_stats()
 
     def get_stats(self):
         stats = googutils.read_from_spreadsheet(SPREADSHEET_ID, "'{}'!{}".format(CURRENT_SHEET, STATS_RANGE), 
                                                 self.sheet_service, 'COLUMNS')
-        return (round(float(stat[1:]), 2) for stat in stats[0]) # total, avg, projected
-
-    def descriptor_to_date(self, raw_date):
-        date_obj = date.today()
-        one_day = timedelta(days=1)
-        if raw_date == 'YESTERDAY':
-            date_obj -= one_day
-        elif raw_date == 'TOMORROW':
-            date_obj += one_day
-        date_str = date.strftime(date_obj, '%m/%d')
-        if date_str[0] == '0':
-            date_str = date_str[1:]
-        return date_str
+        return [round(float(stat[1:]), 2) for stat in stats[0]] # total, avg, projected
+    
+    def cache_dates(self):
+        dates = googutils.read_from_spreadsheet(SPREADSHEET_ID, "'{}'!{}".format(CURRENT_SHEET, DATES_RANGE), 
+                                                self.sheet_service, 'COLUMNS')[0]
+        with open('cached/dates.json', 'w') as f:
+            json.dump(dates, f)
 
     def get_row_for_date(self, date):
         if date.upper() in DESCRIPTORS:
-            date = self.descriptor_to_date(date.upper())
+            date = descriptor_to_date(date.upper())
         offset = self.dates.index(date)
         return offset + ENTRIES_START_ROW
 
@@ -58,11 +55,14 @@ class FoodSpending():
         print('Done!')
         if stats_change:
             _, avg0, proj0 = self.stats
-            _, avg1, proj1 = self.get_stats()
+            new_stats = self.get_stats()
+            _, avg1, proj1 = new_stats
             avg_diff, proj_diff = avg1 - avg0, proj1 - proj0
             sign = '+' if avg_diff >= 0 else '-'
             print('Average:     ${0:.2f} ({1}{2:.2f})'.format(avg1, sign, abs(avg_diff)))   
             print('Projected: ${0:.2f} ({1}{2:.2f})'.format(proj1, sign, abs(proj_diff))) 
+            with open(STATS_CACHE, 'w') as f:
+                json.dump(new_stats, f)
 
     def add_shopping(self, store, amount, date):
         row_num = self.get_row_for_date(date)
@@ -77,6 +77,18 @@ def fetch_meal_type():
         print('({}) {}'.format(i+1, meal_type))
     i = int(input('Select meal type: '))
     return meal_types[i-1]
+
+def descriptor_to_date(raw_date):
+        date_obj = date.today()
+        one_day = timedelta(days=1)
+        if raw_date == 'YESTERDAY':
+            date_obj -= one_day
+        elif raw_date == 'TOMORROW':
+            date_obj += one_day
+        date_str = date.strftime(date_obj, '%m/%d')
+        if date_str[0] == '0':
+            date_str = date_str[1:]
+        return date_str
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
